68531ee4eae39fb4fa7108587aa10f3016215c2d
Xiaoxi Liu <psytky03@gmail.com>
2015-07-22 13:51:13 +0900

    minor correction

--- initial_version/Find_sgRNA.pl
+++ initial_version/Find_sgRNA.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+# A perl program to search for CRISPR-Cas9 PAM sequence 
+# could retrive sequences from UCSC DAS server
+# supports FASTA file input
+# query the off-targes number at super-fast gggenome engine (http://gggenome.dbcls.jp/)
+
 use strict;
 use warnings;
 use 5.010;
@@ -7,26 +12,22 @@ use Getopt::Long;
 use List::Util qw(min max);
 use Term::ANSIColor;
 
-
-our ($fasta_file, $bed_file, $genome, $mode,$length,$max_number, $help, $output);
+our ($fasta_file, $bed_file, $genome, $mode,$length,$max_number, $help);
 
 sub help 
 {print <<USAGE;
 
-Usage:
+--- Usage ---
+perl sgRNAfinder.pl -b [bed file]| -f [fasta file] -g [genome-build] -m [search mode] -l [sgRNA length] -n [top sequences]
 
-perl sgRNAfinder.pl -b [bed file]| -f [fasta file] -o [output file] -g [genome-build] -m [search mode] -l [sgRNA length] -n [top sequences] 
-
-Parameters:
-
--b [the input bed file] 				e.g: example.bed
--f [the input fasta file]				e.g: example.fa
--o [the output file]					e.g: example_out.txt
--g [genome build] 					[default: hg19] | e.g: hg19;mm9;rn5; 
--m [search mode]					[default: s]	| e.g: [s|p] s: single mode; p: paired mode	
--l [sgRNA guide sequence length				[default: 20]	| the default length of guide sequence is 20 nt  
--n [top sequences] 					[default: 10]	| return top 10 sgRNA sequences 
-
+--- Parameters ---
+-b [the bed file] 					e.g: example.bed
+-f [the fasta file]					e.g: example.fa
+-g [genome build] 					e.g: hg19;mm9;rn5;  
+-m [search mode] 					e.g: s|p| s: single mode| p: paired mode
+-l [sgRNA guide sequence length]			e.g: 20 | default 20
+-n [top sequences] 					e.g: 5 	| return top 5 sgRNA sequences
+-o [output file]					e.g: example_out.txt
 -h [help]
 
 USAGE
@@ -34,23 +35,17 @@ exit;
 }
 
 GetOptions (			
-"fasta|f=s"		=> \$fasta_file,
-"bed|b=s"		=> \$bed_file,
-"genome|g=s"	=> \$genome,
-"mode|m=s"		=> \$mode,
-"length|l=i"	=> \$length,
-"max|n=s"		=> \$max_number,
-"help|h"		=> \&help,
-"output|o=s"	=> \$output,
+"fasta|f=s" => \$fasta_file,
+"bed|b=s" => \$bed_file,
+"genome|g=s" => \$genome,
+"mode|m=s" => \$mode,
+"length|l=i" => \$length,
+"max|n=s" => \$max_number,
+"help|h" => \&help,
 ) or die ("\nPlease check the arguments; perl cpfind.pl -h for help\n\n");
 
-
-
-
-defined ($fasta_file or $bed_file or $genome or $mode or $length or $max_number or $help) or print &help;
-if (!$fasta_file and !$bed_file){
-	print "Error: please provide the input file (Bed or Fasta format)\n\n"
-} 
+defined ($fasta_file or $bed_file or $genome or $mode or $length or $max_number or $help) or &help;
+if (!$fasta_file and !$bed_file){print "\nPlease provide the input file (bed or Fasta format)\n\n"} 
 
 
 
